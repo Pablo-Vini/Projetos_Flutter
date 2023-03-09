d640bb015ab850590713afb914995d9383dd06f9
Pablo Vinícius Gonçalves Ramos <124642141+Pablo-Vini@users.noreply.github.com>
2023-03-09 17:55:40 -0300

    Formatação de texto
    Estudando formatação de texto

--- flutter_teste/lib/main.dart
+++ flutter_teste/lib/main.dart
@@ -5,15 +5,22 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MaterialApp(
     title: "Frases do dia",
-    home: Column(
-      children: <Widget>[
-        Text(
-          "Usuário: ",
-          style: TextStyle(color: Colors.black12, fontSize: 20),
-        ),
-        Text("Senha:"),
-        Text("Esqueci minha senha:")
-      ],
+    home: Container(
+      color: Colors.white,
+      child: Column(
+        children: <Widget>[
+          Text(
+            "texto 1 alguma coisa aleatoria para preencher espaço como uma frase beeeem grande",
+            style: TextStyle(
+                fontSize: 35,
+                color: Colors.black,
+                fontWeight: FontWeight.normal,
+                letterSpacing: 0,
+                wordSpacing: 0,
+                decoration: TextDecoration.none),
+          )
+        ],
+      ),
     ),
   ));
 }
